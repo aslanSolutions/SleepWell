451980468a5e1e8e25caa26be142c5db246dd2c2
muhmmad <muhmmad@student.chalmers.se>
2023-05-11 17:35:19 +0200

    "#10 Added class ColorConfigurer to handle the colors"

---
+++ sleep_well/lib/color_configurer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorConfigurer {
+
+  static Color getColor(double progressValue) {
+
+    if (progressValue >= 0.9) {
+      return const Color.fromARGB(255, 8, 220, 15);
+    } else if (progressValue >= 0.7) {
+      return const Color.fromARGB(255, 1, 200, 64);
+    } else if (progressValue >= 0.5) {
+      return Colors.yellow;
+    } else if (progressValue >= 0.4) {
+      return const Color.fromARGB(255, 255, 131, 59);
+    } else {
+      return Colors.red;
+    }
+  }
+}
