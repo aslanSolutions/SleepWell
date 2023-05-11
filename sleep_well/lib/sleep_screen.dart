8803d59f9e92f9db7c53646ac7cc08e8370145ae
yosuefab <gusabbyo@student.gu.se>
2023-05-11 23:50:16 +0200

    #11 SleepLabel added

--- sleep_well/lib/sleep_screen.dart
+++ sleep_well/lib/sleep_screen.dart
@@ -4,6 +4,21 @@ import 'package:slide_to_act/slide_to_act.dart';
 import 'package:lottie/lottie.dart';
 import './labels.dart';
 
+class SleepLabel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Calculating sleep score... ",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          fontSize: 30,
+          color: Color.fromARGB(255, 221, 199, 6),
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic),
+    );
+  }
+}
+
 class SleepScreen extends StatefulWidget {
   const SleepScreen({Key? key}) : super(key: key);
 
