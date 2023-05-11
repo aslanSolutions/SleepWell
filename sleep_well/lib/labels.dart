52a38eea878792858dc45ef6b8e424ebc62f45f2
yosuefab <gusabbyo@student.gu.se>
2023-05-12 01:26:24 +0200

    #11SleepScreenUpdate

--- sleep_well/lib/labels.dart
+++ sleep_well/lib/labels.dart
@@ -11,13 +11,18 @@ class TakeANapLabel extends StatelessWidget {
   }
 }
 
-class SleepLogLabel extends StatelessWidget {
+class SleepLabel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Text(
-      "Sleep Log ",
-      textAlign: TextAlign.left,
-      style: TextStyle(fontSize: 20, color: Colors.black),
+      "Calculating sleep score... ",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          fontSize: 30,
+          color: Color.fromARGB(255, 221, 199, 6),
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic),
     );
   }
 }
+
