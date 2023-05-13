97e3b93323579fa147b4910c71deebeb6a571225
qutaiba <qutaiba@student.chalmers.se>
2023-05-13 03:51:52 +0200

    #13 Refactored app bar into own class "MyAppBar"

---
+++ sleep_well/lib/app_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const MyAppBar({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+    );
+  }
+}
