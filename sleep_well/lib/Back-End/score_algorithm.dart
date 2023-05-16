68c90a759be81f9abd1e186cfc4724c5d711c80b
qutaiba <qutaiba@student.chalmers.se>
2023-05-16 21:51:10 +0200

    #12 implemented loudness algorithm

--- sleep_well/lib/Back-End/score_algorithm.dart
+++ sleep_well/lib/Back-End/score_algorithm.dart
@@ -4,24 +4,31 @@ class ScoreAlgorithm {
   ScoreAlgorithm();
 
   Converter converter = Converter();
-  
-  int calcluateLight (){
-    List <int> lightScoreList = converter.getLight();
+
+  int calcluateLight() {
+    List<int> lightScoreList = converter.getLight();
     int result = 0;
-    for(int i = 0; i < lightScoreList.length-1 ; i++){
-      result = ((result + lightScoreList[i])/i) as int;
+    for (int i = 0; i < lightScoreList.length - 1; i++) {
+      result = ((result + lightScoreList[i]) / i) as int;
     }
     return result;
   }
 
-
-  int calculateTemperature(){
-
-    List <int> tempScoreList = converter.getTemperature();
+  int calculateTemperature() {
+    List<int> tempScoreList = converter.getTemperature();
     int result2 = 0;
-    for(int i = 0; i<tempScoreList.length-1; i++){
-      result2 = ((result2 + tempScoreList[i])/i) as int;
+    for (int i = 0; i < tempScoreList.length - 1; i++) {
+      result2 = ((result2 + tempScoreList[i]) / i) as int;
     }
     return result2;
+  }
+
+  int calculateLoudness() {
+    List<int> loudnessScoreList = converter.getLoudness();
+    int result = 0;
+    for (int i = 0; i < loudnessScoreList.length - 1; i++) {
+      result = ((result + loudnessScoreList[i]) / i) as int;
+    }
+    return result;
   }
 }
