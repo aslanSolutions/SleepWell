b77cd8e33542541ddb44c65c565c03e12e28ac3c
Nawras Kanjo <nawrask@student.chalmers.se>
2023-05-16 19:55:50 +0000

    removed old commented lines

--- sleep_well/lib/log_screen.dart
+++ sleep_well/lib/log_screen.dart
@@ -39,30 +39,3 @@ class LogScreen extends StatelessWidget {
 
 
 
-/*import './log_screen_controller.dart';
-import './values.dart';
-import 'package:flutter/material.dart';
-
-class LogScreen extends StatelessWidget {
-  const LogScreen(this.switchToScore, {super.key});
-  final void Function() switchToScore;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue[300],
-        elevation: 0,
-        title: const Text("S L E E P W E L L"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: LogScreenController(values),
-          ),
-          Expanded(child: LogScreen2(values, switchToScore))
-        ],
-      ),
-    );
-  }
-}*/
