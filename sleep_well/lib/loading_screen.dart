8f213a690a51fe59b5cc93d3b9ed1d2faa8b0b94
muhmmad <muhmmad@student.chalmers.se>
2023-05-17 20:46:30 +0200

    #9 added animation

--- sleep_well/lib/loading_screen.dart
+++ sleep_well/lib/loading_screen.dart
@@ -1,19 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flare_flutter/flare_actor.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeIn,
+    );
+    _animationController.repeat(reverse: true);
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Set your desired background color
+      backgroundColor: Colors.white, 
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Image.asset(
-              'assets/logo.png',
-              width: 150,
-              height: 150,
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: 1.0 - _animation.value,
+                  child: Opacity(
+                    opacity: 1.0 - _animation.value,
+                    child: child,
+                  ),
+                );
+              },
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 250,
+                height: 250,
+              ),
+            ),
+            const SizedBox(height: 16),
+            const SizedBox(
+              width: 250,
+              height: 250,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+              ),
             ),
           ],
         ),
