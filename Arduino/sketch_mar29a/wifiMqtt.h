c0d99320275527ae65f8b333f3bf1b73fd5a623e
elhasan <guselhmu@student.gu.se>
2023-05-21 23:37:27 +0200

    #17 Implemented the wifi connection

--- Arduino/sketch_mar29a/wifiMqtt.h
+++ Arduino/sketch_mar29a/wifiMqtt.h
@@ -1,29 +1,5 @@
 #include "rpcWiFi.h"
 
-const char* ssid = "TP-Link_7FE2_5G";
-const char* password =  "12345678";
+const char* ssid = "SEM";
+const char* password =  "sem2025gu";
 
-void setup() {
-    Serial.begin(115200);
-    while(!Serial); // Wait for Serial to be ready
-
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-
-    Serial.println("Connecting to WiFi..");
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.println("Connecting to WiFi..");
-        WiFi.begin(ssid, password);
-    }
-    Serial.println("Connected to the WiFi network");
-    Serial.print("IP Address: ");
-    Serial.println (WiFi.localIP()); // prints out the device's IP address
-    }
-
-void loop() {
-
-}
